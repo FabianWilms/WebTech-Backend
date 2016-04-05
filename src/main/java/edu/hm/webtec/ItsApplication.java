4df3c959d4219b451be3f0c05edd7f3e4bf4d982
Fabian Holtkötter <fabianholtkoetter@gmail.com>
2016-04-05 16:40:04 +0200

    init spring

---
+++ src/main/java/edu/hm/webtec/ItsApplication.java
@@ -0,0 +1,12 @@
+package edu.hm.webtec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsApplication.class, args);
+	}
+}
