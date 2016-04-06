c33c2346cb592d3c06bd3d485dca99374bd82d7b
Fabian Holtkötter <fabianholtkoetter@gmail.com>
2016-04-06 10:22:15 +0200

    configure maven to build to a war file

--- src/main/java/edu/hm/webtec/ItsApplication.java
+++ src/main/java/edu/hm/webtec/ItsApplication.java
@@ -2,9 +2,17 @@ package edu.hm.webtec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class ItsApplication {
+public class ItsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ItsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItsApplication.class, args);
