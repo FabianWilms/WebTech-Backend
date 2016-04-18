5e54f130e7dcd66ed1c4a4bda9dada67ced6cc9e
peter-mueller <peter7995@gmail.com>
2016-04-18 16:14:16 +0200

    add tests for preflight and allow origin header

--- src/test/java/edu/hm/webtec/configurations/DataRESTCorsConfigurationTest.java
+++ src/test/java/edu/hm/webtec/configurations/DataRESTCorsConfigurationTest.java
@@ -1,13 +1,26 @@
 package edu.hm.webtec.configurations;
 
 import edu.hm.webtec.ItsApplication;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CorsFilter;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author peter-mueller
@@ -17,8 +30,41 @@ import static org.junit.Assert.*;
 @WebAppConfiguration
 public class DataRESTCorsConfigurationTest {
 
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    CorsFilter corsFilter;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
+                .addFilter(corsFilter).build();
+    }
+
+    @Test
+    public void testPreflight() throws Exception {
+        final MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .options("/")
+                .header(HttpHeaders.ORIGIN, "*")
+                .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "GET")
+                .header(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS, "test");
+
+        final ResultMatcher origin = MockMvcResultMatchers
+                .header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        final ResultMatcher methods = MockMvcResultMatchers
+                .header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET,HEAD,POST,PUT,PATCH,DELETE,OPTIONS");
+        final ResultMatcher headers = MockMvcResultMatchers
+                .header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "test");
+        mockMvc.perform(requestBuilder)
+                .andExpect(origin).andExpect(methods).andExpect(headers);
+    }
+
     @Test
     public void testCors() throws Exception {
-        //TODO
+        mockMvc.perform(MockMvcRequestBuilders.get("/").header(HttpHeaders.ORIGIN, "*"))
+          .andExpect(MockMvcResultMatchers.header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*"));
     }
 }
