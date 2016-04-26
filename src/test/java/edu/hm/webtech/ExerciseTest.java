cfc2616e32bdb3862f26f02caebb7cec65e1a796
ToniBianca <toni@hm.edu>
2016-04-26 15:39:16 +0200

    Added Test for Exercise Entity
    # Conflicts:
    #	src/main/java/edu/hm/webtech/ItsApplication.java

---
+++ src/test/java/edu/hm/webtech/ExerciseTest.java
@@ -0,0 +1,66 @@
+package edu.hm.webtech;
+
+import edu.hm.webtech.entities.Exercise;
+import edu.hm.webtech.entities.Topic;
+import edu.hm.webtech.repositories.ExerciseRepository;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * @author Bianca
+ */
+public class ExerciseTest extends ItsApplicationTests {
+    
+    @Autowired
+    WebApplicationContext webApplicationContext;
+    
+    @Autowired
+    ExerciseRepository exerciseRepository;
+    
+    private MockMvc mockMvc;
+    
+    @Before
+    public void setMockUpMVC() {
+         mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    private class ExerciseHelper extends Exercise {
+        public ExerciseHelper() {
+        }
+    }
+    
+    private ExerciseHelper createExercise() {
+        TopicBloomLevel tbl = new TopicBloomLevel();
+        tbl.setBloomLevel(BloomLevel.VERSTEHEN);
+        Topic topic = new Topic();
+        topic.setTopic("Topic");
+        tbl.setTopic(topic);
+        Set<TopicBloomLevel> tbls = new HashSet<>();
+        tbls.add(tbl);
+        ExerciseHelper helper = new ExerciseHelper();
+        helper.setDescription("Exercise");
+        helper.setId(1L);
+        helper.setTopicBloomLevel(tbls);
+        
+        return helper;
+    }
+    
+    @Test
+    public void testCreate() {
+        createExercise();
+    }
+    
+    @Test
+    public void testRequestGet() throws Exception {
+        mockMvc.perform(get("/exercises"))
+                .andExpect(status().isOk());
+    }
+}
