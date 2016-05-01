e33e6080abb0aed067f2af83c085cd5d44abaf06
peter-mueller <peter7995@gmail.com>
2016-05-01 17:08:03 +0200

    forbid findAll

--- src/main/java/edu/hm/webtech/repositories/AssociationRepository.java
+++ src/main/java/edu/hm/webtech/repositories/AssociationRepository.java
@@ -1,8 +1,10 @@
 package edu.hm.webtech.repositories;
 
 import edu.hm.webtech.entities.Association;
+import jdk.Exported;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,4 +12,11 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface AssociationRepository extends CrudRepository<Association, Long> {
+    @Override
+    @RestResource(exported = false)
+    Iterable<Association> findAll();
+
+    @Override
+    @RestResource(exported = false)
+    Iterable<Association> findAll(Iterable<Long> iterable);
 }
